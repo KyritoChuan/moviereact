aece07ad782769323598c10895139bb649e7b15a
KyritoChuan <bladeandsoul18@gmail.com>
2021-01-14 17:57:39 -0300

    Updates

---
+++ precache-manifest.3119476c50f7bd8d3c128379dd57e386.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e226136d5152146a9553f6ab6163c905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691cce0cdb88429bb1cb",
+    "url": "/static/css/2.e75542ce.chunk.css"
+  },
+  {
+    "revision": "4f44446632bf4a0275de",
+    "url": "/static/css/main.f5d0a942.chunk.css"
+  },
+  {
+    "revision": "691cce0cdb88429bb1cb",
+    "url": "/static/js/2.3c0d6848.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3c0d6848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f44446632bf4a0275de",
+    "url": "/static/js/main.abc877f1.chunk.js"
+  },
+  {
+    "revision": "5bd4a91f34a873d7ba81",
+    "url": "/static/js/runtime-main.c3fcd443.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/static/media/logo.cb90c5bd.svg"
+  }
+]);
